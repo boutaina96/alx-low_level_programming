69971c4001b58128509cfd17ca660391c9f16b75
rh Boutaina <rh.boutaina@gmail.com>
2023-03-16 17:03:20 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio>
+/**
+ * main -A program that prints one line with the printf function
+ * Return: 0(success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
