5a3eb14f9e6146aed74b59907c536194e1e66336
rh Boutaina <rh.boutaina@gmail.com>
2023-03-20 16:22:22 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -A program that prints the alphabet in lowercase
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar (ch);
+	return (0);
+}
