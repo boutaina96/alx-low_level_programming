01f9dbafaae1e70223489da6eacb9b8227b4fd4d
rh Boutaina <rh.boutaina@gmail.com>
2023-03-20 18:25:35 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main -Main function
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = 1; m < 10; j++)
+		{
+			if (n < m && n != m)
+			{
+				putchar(n + '0');
+				putchar(m + '0');
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
