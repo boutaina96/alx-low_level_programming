810917c83e918cfade68a77bbf30bb040ab7b795
rh Boutaina <rh.boutaina@gmail.com>
2023-03-20 18:58:01 -0700

    print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main -Main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+	int l;
+
+	for (n = 0; n <= 7; n++)
+	{
+		for (m = 1; m <= 8; m++)
+		{
+			for (l = 3; l <= 9; l++)
+			{
+				if (n < m && m < l)
+				{
+					putchar(n + '0');
+					putchar(m + '0');
+					putchar(l + '0');
+					if (n + m + l != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
