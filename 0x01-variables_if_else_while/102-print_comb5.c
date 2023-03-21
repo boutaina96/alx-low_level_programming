813535f6c6ba4d2bd9a69c60789c47054d6a4c5f
rh Boutaina <rh.boutaina@gmail.com>
2023-03-20 19:15:42 -0700

    comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main -Main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n <= 98; n++)
+	{
+		for (m = n + 1; m <= 99; m++)
+		{
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+			putchar(' ');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			if (n != 98 && m != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
