71450c6b53c3481941c3f4fb06583505364a0d31
rh Boutaina <rh.boutaina@gmail.com>
2023-03-21 17:02:56 -0700

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char n;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+			_putchar(n);
+		_putchar('\n');
+	}
+}
