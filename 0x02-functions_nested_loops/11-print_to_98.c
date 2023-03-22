6d2c7a9bcc6ab24dbae4605d0665b4d8e59c0652
rh Boutaina <rh.boutaina@gmail.com>
2023-03-21 19:31:02 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,11 +16,11 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				printf("%d, ", i);
+				printf("%d, ",i);
 			else
-				printf("%d", i);
+				printf("%d",i);
 		}
-		_putchar('\n');
+		printf("\n");
 	}
 	else if (n < 0)
 	{
